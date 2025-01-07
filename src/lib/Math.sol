a2e02582d23722eab5a9b49151da06cd86195b6e
Ahmet Ahmedov <ahmet@hph.io>
2025-01-07 16:19:20 +0200

    Rewrite DAI stablecoin | Part 8 - Implement modfiyCdp function

--- src/lib/Math.sol
+++ src/lib/Math.sol
@@ -6,7 +6,39 @@ uint256 constant RAY = 10 ** 27;
 uint256 constant RAD = 10 ** 45;
 
 library Math {
-    function add(uint x, int y) internal pure returns (uint) {
-        return y >= 0 ? x + uint(y) : x - uint(-y);
+
+    function min(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        return x <= y ? x : y;
+    }
+
+    function max(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        return x >= y ? x : y;
+    }
+    function add(uint256 x, int256 y) internal pure returns (uint256) {
+        return y >= 0 ? x + uint256(y) : x - uint256(-y);
+    }
+
+    function sub(uint256 x, int256 y) internal pure returns (uint256 z) {
+        z = x - uint256(y);
+        require(y <= 0 || z <= x);
+        require(y >= 0 || z >= x);
+    }
+
+    function mul(uint256 x, int256 y) internal pure returns (int256 z) {
+        z = int256(x) * y;
+        require(int256(x) >= 0);
+        require(y == 0 || z / y == int256(x));
+    }
+
+    function add(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require((z = x + y) >= x);
+    }
+
+    function sub(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require((z = x - y) <= x);
+    }
+
+    function mul(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require(y == 0 || (z = x * y) / y == x);
     }
 }
--- src/stable-coin/CDPEngine.sol
+++ src/stable-coin/CDPEngine.sol
@@ -40,12 +40,14 @@ contract CDPEngine is Auth, CircuitBreaker {
         collaterals[colType].rateAcc = 10 ** 27; // rad
     }
     // file
-    function set(bytes32 key, uint val) external auth notStopped {
+
+    function set(bytes32 key, uint256 val) external auth notStopped {
         if (key == "sysMaxDebt") sysMaxDebt = val;
         else revert("Key not recognized");
     }
     // file
-    function set(bytes32 colType, bytes32 key, uint val) external auth notStopped {
+
+    function set(bytes32 colType, bytes32 key, uint256 val) external auth notStopped {
         if (key == "spot") collaterals[colType].spot = val;
         else if (key == "maxDebt") collaterals[colType].maxDebt = val;
         else if (key == "minDebt") collaterals[colType].minDebt = val;
@@ -81,5 +83,62 @@ contract CDPEngine is Auth, CircuitBreaker {
     // slip
     function modifyCollateralBalance(bytes32 colType, address user, int256 wad) external auth {
         gem[colType][user] = Math.add(gem[colType][user], wad);
+    }
+
+    uint256 public sysDebt;
+
+    // frob - modify a CDP
+    function modifyCdp(
+        // i - collateral id
+        bytes32 colType,
+        // u - address that maps to CDP
+        address cdp,
+        // v - source of gem
+        address gemSrc,
+        // w - destination of coin
+        address coinDst,
+        // dink - delta collateral
+        int256 deltaCol,
+        // dart - delta debt
+        int256 deltaDebt
+    ) external notStopped {
+        Position memory pos = positions[colType][cdp];
+        Collateral memory col = collaterals[colType];
+        // ilk has been initialised
+        require(col.rateAcc != 0, "collateral not initialized");
+
+        pos.collateral = Math.add(pos.collateral, deltaCol);
+        pos.debt = Math.add(pos.debt, deltaDebt);
+        col.debt = Math.add(col.debt, deltaDebt);
+
+        // dtab
+        int256 deltaCoin = Math.mul(col.rateAcc, deltaDebt);
+        // tab
+        uint256 coinDebt = col.rateAcc * pos.debt;
+        sysDebt = Math.add(sysDebt, deltaCoin);
+
+        // either debt has decreased, or debt ceilings are not exceeded
+        require(
+            deltaDebt <= 0 || col.debt * col.rateAcc <= col.maxDebt && sysDebt <= sysMaxDebt,
+            "Vat/ceiling-exceeded"
+        );
+        // pos is either less risky than before, or it is safe
+        require((deltaDebt <= 0 && deltaCol >= 0) || (coinDebt <= pos.collateral * col.spot), "Vat/not-safe");
+
+        // pos is either more safe, or the owner consents
+        require((deltaDebt <= 0 && deltaCol >= 0) || canModifyAccount(cdp, msg.sender), "Vat/not-allowed-u");
+        // collateral src consents
+        require(deltaCol <= 0 || canModifyAccount(gemSrc, msg.sender), "Vat/not-allowed-v");
+        // debt dst consents
+        require(deltaDebt >= 0 || canModifyAccount(coinDst, msg.sender), "Vat/not-allowed-w");
+
+        // urn has no debt, or a non-dusty amount
+        require(pos.debt == 0 || coinDebt >= col.minDebt, "Vat/dust");
+
+        gem[colType][gemSrc] = Math.sub(gem[colType][gemSrc], deltaCol);
+        coin[coinDst] = Math.add(coin[coinDst], deltaCoin);
+
+        positions[colType][cdp] = pos;
+        collaterals[colType] = col;
     }
 }
